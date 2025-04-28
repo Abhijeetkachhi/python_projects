bd30218e7426f57a1e2cc80939edaa9b4f1f2018
Abhijeet patel <abhijeetpatelsagar@gmail.com>
2025-04-28 13:35:33 +0530

    Initial commit

--- README.py
+++ README.py
@@ -1,1 +1,8 @@
-print("hello this the first python program")
+import re
+# This is the simple email collection with right pattern
+email= input("what is your eamil").strip()
+
+if re.search(r"^\w+@(\w\.)?\w+\.com$", email, re.IGNORECASE):
+    print("valid")
+else:
+    print("invalid")
