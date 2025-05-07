c071727cb5f0d64a92d04ac60ec5890784c7b999
Abhijeet patel <abhijeetpatelsagar@gmail.com>
2025-05-07 10:32:24 +0530

    Initial commit

--- oracle2.sql
+++ oracle2.sql
@@ -60,6 +60,91 @@ END;
 /
 SET SERVEROUTPUT OFF
 
+-- THIS IS USE FOR LOOP OR LOOPING COMMNADS
 
+-- this is use of for loop
+ser SERVEROUTPUT on
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('this is a for loop');
+    for i in 1..5 LOOP //// for i  in reverse 1..5 loop     -- this is another way to reverse the loop
+        DBMS_OUTPUT.PUT_LINE('number is :'||  i);
+    end loop;
+END;
+/
+set SERVEROUTPUT OFF
 
+--- this is a loop in oracle
+
+set SERVEROUTPUT on 
+    DECLARE
+        i number(2):=3;
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('this is demo on loop');
+        loop
+            exit when i>10;
+            DBMS_OUTPUT.PUT_LINE('number is :'||i);
+            i:=i + 1;
+        end loop;
+    end;
+    /
+set SERVEROUTPUT OFF
+
+-- this is a while loop in oracle
+set SERVEROUTPUT on 
+    DECLARE
+        i number(2):=1;
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('this is a while loop');
+        while i<= 10 loop
+            DBMS_OUTPUT.PUT_LINE('number is:'|| i);
+        end loop;
+    END;
+    /
+set SERVEROUTPUT OFF
+
+-- this is a cursor in oracle
+set SERVEROUTPUT on 
+set verify OFF
+    DECLARE
+        ctr number(2):=0;
+        deid number(2);
+        dename varchar(3);
+        dlocation varchar2(34);
+
+    cursor curso_1 is
+        SELECT eid , ename , location from empolore where Dnmae ='TA';
+        no_department_found exception;
     
+    BEGIN
+        OPEN curso_1;
+        LOOP
+            FETCH curso_1 INTO DEID, DNAME, DLOCATION;
+            EXIT WHEN CUR_EXP%no_department_found
+            CTR i = ctr + 1;
+            insert into newemployee values (deid , dname, dlocation);
+            DBMS_OUTPUT.PUT_LINE(ctr|| 'lines inserted into newempoyee table');
+        end loop;
+    
+         DBMS_OUTPUT.PUT_LINE('total number of records is '|| ctr);
+
+    end;
+    /
+    set SERVEROUTPUT OFF
+set verify on
+
+-- this is a trigger in oracle
+create trigger trigger_name
+    after /// before insert or update or delete on table_name
+        for each row
+            begin
+                if inserting THEN
+                    insert into table_nmae_2 values(cloumn_nmae);
+                elseif update THEN
+                insert into table_nmae_2 values(cloumn_nmae);
+                elseif delete THEN
+                insert into table_nmae_2 values(cloumn_nmae);
+            end if;
+        end;
+        /
+        
+
